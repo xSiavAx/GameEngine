7d23f8da8ce3c36dd502eb2c623b2d6bb5f3c0a6
Stanislav Dmytriiev <stanislav.dmytriiev@shortcut.no>
2025-02-03 19:46:30 -0500

    Add GL mouse cursor subscribe

--- GameEngine/Sources/GameEngine/Input/Mouse/InputProcessorMouse.swift
+++ GameEngine/Sources/GameEngine/Input/Mouse/InputProcessorMouse.swift
@@ -1,4 +1,5 @@
 import OpenCombineShim
+import C_GLFW
 
 extension InputProcessor {
     final class Mouse {
@@ -27,15 +28,28 @@ extension InputProcessor {
         private func add(posObserver: PosObserver) {
             posObservers.append(WeakWrapper(wrappe: posObserver))
             if posObservers.count == 1 {
-                // Subscribe
+                glfwSetCursorPosCallback(windowPtr, cursopr_pos_callback)
             }
         }
 
         private func remove(posObserver: PosObserver) {
             posObservers.removeAll { $0.wrappe === posObserver }
             if posObservers.count == 0 {
-                // Unsubscribe
+                glfwSetCursorPosCallback(windowPtr, nil)
             }
         }
+
+        fileprivate func handleMousePos(x: Double, y: Double) {
+            posObservers
+                .compactMap(\.wrappe)
+                .forEach {
+                    $0.fire(x: x, y: y)
+                }
+        }
     }
+}
+
+private func cursopr_pos_callback(window: OpaquePointer?, x: Double, y: Double) {
+    guard let window, let window = WindowsManager.shared.get(key: window) else { return }
+    window.inputProcessor.mouse.handleMousePos(x: x, y: y)
 }
